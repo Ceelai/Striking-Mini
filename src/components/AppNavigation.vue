819e57a8d536fb2f85bf2b99e42c99d294f0dbe7
Calvin <laica@sheridancollege.ca>
2020-06-17 16:30:20 -0400

    Initial Scaffolding Created

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-toolbar app color="" dark>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation'
+};
+</script>
+
+<style scoped></style>
