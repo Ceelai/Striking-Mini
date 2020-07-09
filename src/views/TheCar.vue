0eb00fbec2877bb65740d153f3f40ce74100dde5
Calvin <laica@sheridancollege.ca>
2020-07-09 15:55:15 -0400

    Update TheCar.vue

--- src/views/TheCar.vue
+++ src/views/TheCar.vue
@@ -1,18 +1,24 @@
 <template>
   <v-container class="container" fluid dark>
     <v-row justify="space-around">
-      <v-col cols="6">
-        <div class="white--text">
-          In 1959, Alec Issigonis was asked by BMC (British Motor Corporation)
-          to develop a range of family cars that are extremely fuel efficient
-          due to the fuel shortage caused by the 1956 Suez Crisis. One of these
-          designs was what is now more commonly known as the classic Mini. While
-          there there were many configurations of the compact vehicle, the
-          overall asthetics remained the same throughout its entire lifetime.
+      <v-col cols="6" class="txt-box">
+        <div class="white--text display-3 text-center">
+          Classical Elegance
         </div>
+        <v-layout class="align-center pa-3 ml-3 justify-center">
+          <div class="white--text headline ">
+            In 1959, Alec Issigonis was asked by BMC (British Motor Corporation)
+            to develop a range of family cars that are extremely fuel efficient
+            due to the fuel shortage caused by the 1956 Suez Crisis. One of
+            these designs was what is now more commonly known as the classic
+            Mini. While there there were many configurations of the compact
+            vehicle, the overall asthetics remained the same throughout its
+            entire lifetime.
+          </div>
+        </v-layout>
       </v-col>
       <v-col cols="6" class="mini">
-        <v-img src="../assets/2ndMini.jpg" max-height="60vh" max-width=""></v-img>
+        <v-img src="../assets/2ndMini.jpg" max-height="60vh" max-width></v-img>
       </v-col>
     </v-row>
   </v-container>
@@ -29,7 +35,10 @@ export default {
 
 <style scoped lang="scss">
 .container {
-  background-color: rgb(112, 112, 112);
+  background-color: #343434;
   opacity: 78%;
+}
+
+.txt-box{
 }
 </style>
