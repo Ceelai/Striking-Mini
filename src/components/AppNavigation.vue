ad60a131f042370211b5070b68470e7b4aeac03b
Calvin <laica@sheridancollege.ca>
2020-10-17 17:08:47 -0400

    Update AppNavigation.vue
    Added Navbar

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,35 +1,63 @@
 <template>
-  <v-toolbar flat>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-tabs right>
-      <v-tab>Item</v-tab>
-    </v-tabs>
-    <v-btn icon to="/thecar">
-      <v-icon>mdi-car</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>mdi-dots-vertical</v-icon>
-    </v-btn>
-  </v-toolbar>
+  <div>
+    <v-app-bar id="top-nav-bar" height="50" app color="white" flat>
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+      <v-toolbar-title class="title">{{ appTitle }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div>
+      <v-tabs class="hidden-sm-and-down" right optional>
+        <v-tab
+          v-for="(name, i) in tabs"
+          :key="i"
+          :to="{ name }"
+          :exact="name === 'Home'"
+          :ripple="false"
+          active-class="text--primary"
+          class="font-weight-bold"
+          min-width="75"
+          text
+        >
+          {{ name }}
+        </v-tab>
+      </v-tabs>
+      </div>
+      <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer" />
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AppNavigation",
+  props: {},
   data() {
     return {
       appTitle: "Mini",
-      drawer: false,
-      items: []
+      drawer: null,
+      isActive: false,
+      tabs: ["Home", "TheCar", "Variations"]
     };
-  }
+  },
+  methods: {},
+  mounted() {}
 };
 </script>
 
 <style scoped lang="scss">
-.v-toolbar {
-  flex: none;
+.title {
+  overflow: auto;
+}
+
+#top-nav-bar{
+  .v-tabs-slider{
+    max-width: 24px;
+    margin: 0 auto;
+  }
+
+  .v-tab{
+    &::before{
+      display: none;
+    }
+  }
 }
 </style>
