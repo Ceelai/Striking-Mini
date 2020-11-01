d674fa550788f9140041932fc9c9f5580e9150c3
Calvin <calvinlai.quantum@gmail.com>
2020-11-01 14:35:39 -0500

    vue deploy file

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,16 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'calvinlai.page' > CNAME
+echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ceelai/Striking-Mini.git master:gh-pages
+git push -f git@github.com:Ceelai/Striking-Mini.git master:gh-pages
 
 cd -
